1b3d9c6a210cd2df1b6c978582bfcf63aeb62724
minotaur487 <bhar@LAPTOP-HB1336CE>
2024-04-06 16:23:59 -0700

    lottery

--- app/RedeemRaffle/page.tsx
+++ app/RedeemRaffle/page.tsx
@@ -1,42 +1,104 @@
-import { ComponentClass } from "react";
+"use client"
+import React, { useState } from "react";
 
-type RaffleData = {
-    id: string;
-    name: string;
-    description: string;
-    location: string;
-};
+function RaffleGame({setInputOne, setInputTwo, setInputThree, setInputFour, setInputFive}: {setInputOne: any; setInputTwo: any; setInputThree: any; setInputFour: any; setInputFive: any;}) {
+    const handleInputChangeOne= (event: any) => {
+        setInputOne(event.target.value)
+    }
+    const handleInputChangeTwo= (event: any) => {
+        setInputTwo(event.target.value)
+    }
+    const handleInputChangeThree= (event: any) => {
+        setInputThree(event.target.value)
+    }
+    const handleInputChangeFour= (event: any) => {
+        setInputFour(event.target.value)
+    }
+    const handleInputChangeFive= (event: any) => {
+        setInputFive(event.target.value)
+    }
 
-function RaffleGame() {
-    return (
-        <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-            <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-zip">
-                Pick Five Numbers
-            </label>
-            <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-zip" type="text" placeholder="0-9">
-            <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-zip" type="text" placeholder="0-9">
-            <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-zip" type="text" placeholder="0-9">
-            <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-zip" type="text" placeholder="0-9">
-            <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-zip" type="text" placeholder="0-9">
+    return <div className="w-full">
+        <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-zip">
+            Pick Five Numbers
+        </label>
+        <div className="w-full flex flex-row justify-center">
+            <input onChange={(event) => handleInputChangeOne(event)} className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded p-5 m-5 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-zip" type="text" placeholder="0-9"></input>
+            <input onChange={(event) => handleInputChangeOne(event)} className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded p-5 m-5 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-zip" type="text" placeholder="0-9"></input>
+            <input onChange={(event) => handleInputChangeOne(event)} className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded p-5 m-5 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-zip" type="text" placeholder="0-9"></input>
+            <input onChange={(event) => handleInputChangeOne(event)} className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded p-5 m-5 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-zip" type="text" placeholder="0-9"></input>
+            <input onChange={(event) => handleInputChangeOne(event)} className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded p-5 m-5 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-zip" type="text" placeholder="0-9"></input>
         </div>
+    </div>
+}
+
+function LotteryView({randomOne, randomTwo, randomThree, randomFour, randomFive }: {randomOne: number; randomTwo: number, randomThree: number; randomFour: number; randomFive: number; }) {
+    return (
+        <div>
+            <p className="font-bold text-xl mb-2">Lottery Numbers</p>
+            <div className="w-full flex flex-row justify-center">
+                <div className="mx-5">{randomOne}</div>
+                <div className="mx-5">{randomTwo}</div>
+                <div className="mx-5">{randomThree}</div>
+                <div className="mx-5">{randomFour}</div>
+                <div className="mx-5">{randomFive}</div>
+            </div>
+        </div>
+        
     );
 }
 
+function getRandomInt(max: number) {
+    return Math.floor(Math.random() * max);
+  }
+
 function RaffleCard({cost}: {cost: number }) {
+    const [randomOne, setRandomOne] = useState(0);
+    const [randomTwo, setRandomTwo] = useState(0);
+    const [randomThree, setRandomThree] = useState(0);
+    const [randomFour, setRandomFour] = useState(0);
+    const [randomFive, setRandomFive] = useState(0);
+
+    const [inputOne, setInputOne] = useState(0);
+    const [inputTwo, setInputTwo] = useState(0);
+    const [inputThree, setInputThree] = useState(0);
+    const [inputFour, setInputFour] = useState(0);
+    const [inputFive, setInputFive] = useState(0);
+
+    const onClickBuyRaffle = (cost: number, balance: number) => {
+        if (balance < cost) {
+            return;
+        }
+    
+        setRandomOne(getRandomInt(9));
+        setRandomTwo(getRandomInt(9));
+        setRandomThree(getRandomInt(9));
+        setRandomFour(getRandomInt(9));
+        setRandomFive(getRandomInt(9));
+
+        if (inputOne == randomOne && inputTwo == randomTwo &&
+            inputThree == randomThree && inputFour == randomFour
+            && inputFive == randomFive) {
+            // indicate win TODO
+            return
+        }
+
+        // indicate loss
+    }
+
+    const balance = -1;// to be updated
     return (
-        <>
-            <div className="max-w-sm rounded overflow-hidden shadow-lg">
-                <RaffleGame />
-                <div className="px-6 py-4">
-                    <div className="font-bold text-xl mb-2">Coin Flip</div>
-                    <button>Buy Raffle</button>
-                    <RaffleGame></RaffleGame>
-                    <p className="text-gray-700 text-base">
-                        Costs {cost}
-                    </p>
-                </div>
+        <div className="w-full rounded overflow-hidden shadow-lg">
+            <div className="w-full">
+                <div className="font-bold text-xl mb-2">Instant Lottery</div>
+                <p className="text-gray-700 text-base">
+                    Costs {cost}
+                </p>
+                <button onClick={() => onClickBuyRaffle(cost, balance)} className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">Buy Raffle</button>
+                <RaffleGame setInputOne={setInputOne} setInputTwo={setInputTwo} setInputThree={setInputThree} setInputFour={setInputFour} setInputFive={setInputFive}></RaffleGame>
             </div>
-        </>
+            <LotteryView randomOne={randomOne} randomTwo={randomTwo} randomThree={randomThree} randomFour={randomFour} randomFive={randomFive}></LotteryView>
+        </div>
     );
 }
 
