d5e0fc1279cea990d86a43679b1067efd4adf244
Colin Zhao <colinzhao@Colins-MacBook-Pro-Toppings.local>
2024-04-06 16:21:53 -0700

    add currency

---
+++ prisma/migrations/20240406232139_currency/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "Gems" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "amount" INTEGER NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "Cents" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "amount" INTEGER NOT NULL
+);
