316077ff2c509051f4b98213f12d639837fc7935
minotaur487 <63748624+minotaur487@users.noreply.github.com>
2024-04-06 14:00:42 -0700

    work

--- app/RedeemRaffle/page.tsx
+++ app/RedeemRaffle/page.tsx
@@ -1,37 +1,47 @@
 import { ComponentClass } from "react";
 
 type RaffleData = {
-  id: string;
-  name: string;
-  description: string;
-  location: string;
+    id: string;
+    name: string;
+    description: string;
+    location: string;
 };
 
-export default function RedeemRaffle() {
-  return (
-    RaffleCard({cost: 1})
-  )
-}
-
 function RaffleGame() {
-  return (
-    <div class="w-full">
-      game
-    </div>
-  );
+    return (
+        <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+            <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-zip">
+                Pick Five Numbers
+            </label>
+            <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-zip" type="text" placeholder="0-9">
+            <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-zip" type="text" placeholder="0-9">
+            <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-zip" type="text" placeholder="0-9">
+            <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-zip" type="text" placeholder="0-9">
+            <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-zip" type="text" placeholder="0-9">
+        </div>
+    );
 }
 
-function RaffleCard({ cost }: { cost: number }) {
-  return (
-    <div class="max-w-sm rounded overflow-hidden shadow-lg">
-      <RaffleGame/>
-      <div class="px-6 py-4">
-        <div class="font-bold text-xl mb-2">Coin Flip</div>
-        <button>Buy Raffle</button>
-        <p class="text-gray-700 text-base">
-          Costs {cost}
-        </p>
-      </div>
-    </div>
-  );
+function RaffleCard({cost}: {cost: number }) {
+    return (
+        <>
+            <div className="max-w-sm rounded overflow-hidden shadow-lg">
+                <RaffleGame />
+                <div className="px-6 py-4">
+                    <div className="font-bold text-xl mb-2">Coin Flip</div>
+                    <button>Buy Raffle</button>
+                    <RaffleGame></RaffleGame>
+                    <p className="text-gray-700 text-base">
+                        Costs {cost}
+                    </p>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default function RedeemRaffle() {
+    return (
+        <RaffleCard cost={1} />
+    );
 }
