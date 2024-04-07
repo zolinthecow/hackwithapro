0073164f71606f24dd1c330521cf955add1b4bdd
Stephen Schuster <46509946+Stephen-Schuster@users.noreply.github.com>
2024-04-06 17:51:59 -0700

    added getGems and getCents

---
+++ actions/getCents.ts
@@ -0,0 +1,14 @@
+'use server'
+import prisma from "@/prisma";
+async function getCentsAmountByUserId(userId:string) {
+  const centsRecord = await prisma.cents.findUnique({
+    where: {
+      userId: userId,
+    },
+    select: {
+      amount: true, // Select only the amount
+    },
+  });
+
+    return centsRecord.amount;
+}
---
+++ actions/getGems.ts
@@ -0,0 +1,14 @@
+'use server'
+import prisma from "@/prisma";
+async function getGemsAmountByUserId(userId:string) {
+  const gemsRecord = await prisma.gems.findUnique({
+    where: {
+      userId: userId,
+    },
+    select: {
+      amount: true, // Select only the amount
+    },
+  });
+
+    return gemsRecord.amount;
+}
