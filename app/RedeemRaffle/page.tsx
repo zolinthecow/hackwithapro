8b93047d18d723461a593b3acdb448798203504b
AnthonyChui <154588467+AnthonyChui@users.noreply.github.com>
2024-04-06 19:47:45 -0700

    Update page.tsx

--- app/RedeemRaffle/page.tsx
+++ app/RedeemRaffle/page.tsx
@@ -201,7 +201,7 @@ export default function RedeemRaffle() {
             <nav className="flex items-center gap-4 ml-auto">
                 <Link
                 className="font-medium text-gray-900  dark:text-gray-50"
-                href="#"
+                href="/"
                 >
                 <div className="tab-button">
                 Classes
