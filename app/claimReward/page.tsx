bccbdef62ee962506a52445e8fc2d919a030f1ca
Stephen Schuster <46509946+Stephen-Schuster@users.noreply.github.com>
2024-04-06 18:39:07 -0700

    made it so claimreward gives you gems

--- app/claimReward/page.tsx
+++ app/claimReward/page.tsx
@@ -1,14 +1,20 @@
 import prisma from "@/prisma";
 import ClaimRewardView from "./ClaimRewardView";
+import { getSession } from "@auth0/nextjs-auth0";
 
 export default async function ClaimReward() {
+  const session = await getSession();
+  const userId = session?.user.sub;
   const classes = await prisma.class.findMany({
     include: {
       location: true,
       classTimes: true,
     },
+    where: {
+      userId,
+    },
   });
 
-  return <ClaimRewardView classes={classes}/>;
+  return <ClaimRewardView classes={classes} userId={userId/>;
 }
 
